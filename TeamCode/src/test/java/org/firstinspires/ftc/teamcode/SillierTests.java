e4873a1a6f9df3b3957c2b168de98ae6c27a55de
GLRoylance <GLRoylance@gmail.com>
2023-11-25 22:09:52 -0800

    get jUnit 4 working

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/SillierTests.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.junit.Assert.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+// import org.junit.jupiter.api.Test;
+
+public class SillierTests {
+    @Test
+    public void additionTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void additionFailTest() {
+        assertEquals(3, 2 + 2);
+    }
+}
